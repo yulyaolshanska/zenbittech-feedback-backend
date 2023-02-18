c790b10fd3ea0ee9af7028be37d845db792bbcb1
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-02-18 13:40:12 +0200

    try fix cors

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,19 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  });
-  const options = {
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  };
+  const app = await NestFactory.create(AppModule);
+  // , {
+  //   cors: true,
+  // }
+  // const options = {
+  //   origin: true,
+  //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  //   preflightContinue: false,
+  //   optionsSuccessStatus: 204,
+  //   credentials: true,
+  // };
 
-  app.enableCors(options);
+  app.enableCors();
   await app.listen(3001);
 }
 bootstrap();
