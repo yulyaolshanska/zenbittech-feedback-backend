84bf9e7247f5318ae767de0ba10d7bc51f64b6f7
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-02-18 20:57:44 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // , {
-  //   cors: true,
-  // }
+
   // const options = {
   //   origin: true,
   //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
