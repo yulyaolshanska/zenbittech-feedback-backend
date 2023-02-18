121e3376926d1cb664157b0727555297510bdbf7
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-02-18 02:29:09 +0200

    fix cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { Feedback } from "./feedback/feedback.entity";
       port: +process.env.DATABASE_PORT,
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
-      // database: process.env.DATABASE_NAME,
+      database: process.env.DATABASE_NAME,
       entities: [Feedback],
       synchronize: true,
     }),
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,18 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+  const options = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+
+  app.enableCors(options);
   await app.listen(3001);
 }
 bootstrap();
